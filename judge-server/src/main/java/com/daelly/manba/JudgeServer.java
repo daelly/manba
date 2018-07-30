58d274399a94768d6960575f52492ba8ab731c26
liqingyang <liqingyang@yy.com>
2018-07-30 16:56:38 +0800

    Initial commit

---
+++ judge-server/src/main/java/com/daelly/manba/JudgeServer.java
@@ -0,0 +1,14 @@
+package com.daelly.manba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class JudgeServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JudgeServer.class, args);
+    }
+}
